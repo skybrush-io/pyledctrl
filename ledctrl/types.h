3decf2a13000cea5d2eaea2edeb8694b6bac9a3f
Gabor Vasarhelyi <vasarhelyi@angel.elte.hu>
2016-10-07 11:44:19 +0000

    small bugfix for avoiding double include on windows

--- ledctrl/types.h
+++ ledctrl/types.h
@@ -15,8 +15,10 @@ extern "C" {
 
 typedef int8_t s8;
 typedef uint8_t u8;
+#if defined(__unix__) || defined(__APPLE__)
 typedef int16_t s16;
 typedef uint16_t u16;
+#endif
 
 /**
  * Structure for storing a byte range.
