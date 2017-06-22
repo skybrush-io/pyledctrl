8c027e0325746101fd74289a3abdf0aa017e8e1d
Tamas Nepusz <ntamas@gmail.com>
2017-06-22 17:05:43 +0200

    Marker handling is now offloaded to TimestampedLineCollector

--- pyledctrl/compiler/utils.py
+++ pyledctrl/compiler/utils.py
@@ -1,5 +1,6 @@
 """Utility objects and functions for the compiler."""
 
+from bisect import bisect, bisect_left
 from time import time
 
 
@@ -70,17 +71,25 @@ class TimestampedLineCollector(object):
     ``.led`` output file and also keep track of a running timer such that
     each added line is timestamped with the state of the timer at the
     time the line will be executed.
+
+    Additionally, the object may keep track of a set of markers that will
+    be inserted into the collection of lines when the timer passes a given
+    timestamp.
     """
 
-    def __init__(self, fps=25):
+    def __init__(self, out, fps=25):
         """Constructor.
 
         Parameters:
+            out (file-like): file-like object to write the collected lines
+                into
             fps (int): number of frames per second
         """
         self._add_timestamps = True
-        self._lines = []
+        self._markers = []
+        self._marker_index = 0
         self._timer = 0
+        self.out = out
         self.fps = fps
 
     def add(self, line, duration):
@@ -91,34 +100,68 @@ class TimestampedLineCollector(object):
             duration (int): the duration of the execution of the line,
                 in frames
         """
-        self._lines.append((line, self._timer))
-        self._timer += duration
+        if duration > 0:
+            self._print_markers_until(self._timer + 1)
 
-    def flush(self, fp):
-        """Flushes the lines collected so far into the given file-like
-        object.
+        self.out.write(self._format_line(line, self._timer))
+        self._advance_by(duration)
+
+    def add_marker(self, marker, time):
+        """Adds a marker object to be inserted into the collection of lines
+        when the internal timer passes the given timestamp.
+
+        Parameters:
+            marker (str): the marker line to insert when the time comes
+            time (int): the timestamp corresponding to the marker line,
+                in frames
         """
-        for line, timestamp in self._lines:
-            fp.write(self._format_line(line, timestamp))
-        self._lines = []
+        item = time, marker
+        index = bisect(self._markers, item)
+        self._markers.insert(index, item)
+        if index < self._marker_index:
+            self._marker_index += 1
+        elif index == self._marker_index:
+            if self._markers[index][0] <= self._timer:
+                self._marker_index += 1
+
+    def close(self):
+        """Closes the line collector object and flushes all remaining
+        markers into the output.
+        """
+        if self._markers:
+            max_time = max(time for time, _ in self._markers) + 1
+            if max_time > self._timer:
+                self._advance_to(max_time)
 
     @property
     def timer(self):
         """The state of the current timer, in frames."""
         return self._timer
 
-    def _format_line(self, line, timestamp):
+    def _advance_by(self, frames):
+        next_timer = self._timer + frames
+        self._print_markers_until(next_timer)
+        self._timer = next_timer
+
+    def _advance_to(self, frame):
+        diff = frame - self._timer
+        if diff < 0:
+            raise ValueError("Cannot go back in time")
+        self._advance_by(diff)
+
+    def _format_line(self, line, timestamp=None):
         """Formats the given line in a way that is suitable for the
         output.
 
         Parameters:
             line (str): the line to format
-            timestamp (int): the timestamp of the line, in frames
+            timestamp (Optional[int]): the timestamp of the line, in frames,
+                or ``None`` if the line has no timestamp
 
         Returns:
             str: the formatted line
         """
-        if self._add_timestamps:
+        if self._add_timestamps and timestamp is not None:
             if len(line) < 60:
                 line += " " * (60 - len(line))
             line += "# " + self._format_frame_count_as_time(timestamp)
@@ -131,3 +174,10 @@ class TimestampedLineCollector(object):
             minutes=int(minutes), seconds=int(seconds),
             residual=int(residual), frames=int(frames)
         )
+
+    def _print_markers_until(self, timer):
+        next_index = bisect_left(self._markers, (timer, None))
+        for i in xrange(self._marker_index, next_index):
+            timestamp, marker = self._markers[i]
+            self.out.write(self._format_line(marker))
+        self._marker_index = next_index
