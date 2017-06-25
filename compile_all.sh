d9308cad2ab362c8bbd8bbede7037d452ee6bbda
Tamas Nepusz <ntamas@gmail.com>
2017-06-25 21:40:14 +0200

    compile_all.sh: compile only data/simple for the time being

--- compile_all.sh
+++ compile_all.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # 
-# Compiles all the files found in data/
+# Compiles all the files found in data/simple/
 
-for fname in `find data -name '*.led' -type f`; do
+for fname in `find data/simple -name '*.led' -type f`; do
 	outfname="`echo $fname | sed -e 's/^data\//out\//' | sed -e 's/\.led$/.bin/'`"
 	mkdir -p "`dirname $outfname`"
 	bin/ledctrl compile "$fname" -o "$outfname" "$@"
