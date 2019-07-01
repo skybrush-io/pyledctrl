927fa6c0d4d7ec2e8996d8162c7a106e18100112
Tamas Nepusz <ntamas@gmail.com>
2019-07-01 15:49:26 +0200

    added missing packages to setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="Bytecode compiler and utilities for ledctrl",
     author=pyledctrl.__author__,
     author_email=pyledctrl.__email__,
-    packages=["pyledctrl"],
+    packages=["pyledctrl", "pyledctrl.compiler", "pyledctrl.parsers"],
     scripts=["bin/ledctrl"],
     requires=["pyserial (>=3.1.1)", "Baker (>=1.0)", "tqdm (>= 4.8.4)", "lxml (>= 3.6.4)"]
 )
