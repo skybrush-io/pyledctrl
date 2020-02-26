bfa1827bad3841f7ba5b4e25d0667ab9d9e9f3c2
Tamas Nepusz <ntamas@gmail.com>
2020-02-26 23:50:13 +0100

    bumped version to 3.0.0

--- pyledctrl/version.py
+++ pyledctrl/version.py
@@ -1,4 +1,4 @@
 __author__ = "Tamas Nepusz"
 __email__ = "ntamas@gmail.com"
-__version_info__ = (2, 4, 3)
+__version_info__ = (3, 0, 0)
 __version__ = ".".join("{0}".format(x) for x in __version_info__)
