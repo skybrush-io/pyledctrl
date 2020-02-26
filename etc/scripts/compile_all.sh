6932f26ebd3f94b7bbb100ff2025762eb0b2ac39
Tamas Nepusz <ntamas@gmail.com>
2020-02-26 07:51:19 +0100

    moved compile_all.sh to etc/scripts

--- etc/scripts/compile_all.sh
+++ etc/scripts/compile_all.sh
@@ -2,6 +2,10 @@
 # 
 # Compiles all the files found in data/simple/ and data/tests/
 
+SCRIPT_ROOT=`dirname $0`
+
+cd "${SCRIPT_ROOT}/../.."
+
 for subdir in simple tests; do
 	for fname in `find data/$subdir -name '*.led' -type f`; do
 		outfname="`echo $fname | sed -e 's/^data\//out\//' | sed -e 's/\.led$/.bin/'`"
