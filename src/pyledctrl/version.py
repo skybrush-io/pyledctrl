4e7fdf10bd993ea61efcd19e5390ca1a9674d899
Tamas Nepusz <ntamas@gmail.com>
2021-05-23 01:28:14 +0200

    chore: bumped version to 3.2.0

--- src/pyledctrl/version.py
+++ src/pyledctrl/version.py
@@ -1,4 +1,4 @@
 __author__ = "Tamas Nepusz"
 __email__ = "tamas@collmot.com"
-__version_info__ = (3, 1, 1)
+__version_info__ = (3, 2, 0)
 __version__ = ".".join("{0}".format(x) for x in __version_info__)
